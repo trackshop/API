3e88fe2073bad636e4b7a8a7fd22cb8f62c18ce6
Th7mo <thimoquinten@gmail.com>
2021-12-18 16:37:33 +0100

    initial commit

---
+++ src/main/java/nl/th7mo/trackshop/api/Application.java
@@ -0,0 +1,12 @@
+package nl.th7mo.trackshop.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/nl/th7mo/trackshop/api/ApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.th7mo.trackshop.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
