d611aae3803cadf4b5399e05fc07286d7a8f9670
th7mo <dev@th7mo.com>
2021-12-19 14:11:45 +0100

    Made DAO for retreiving access_token

--- src/main/java/nl/th7mo/trackshop/api/Application.java
+++ src/main/java/nl/th7mo/trackshop/api/Application.java
@@ -1,12 +1,17 @@
 package nl.th7mo.trackshop.api;
 
+import nl.th7mo.trackshop.api.spotify.access_token.AccessTokenDAO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        Logger.getLogger("main").log(Level.WARNING, AccessTokenDAO.get().accessToken);
     }
 }
---
+++ src/main/java/nl/th7mo/trackshop/api/spotify/access_token/CredentialEncoder.java
@@ -0,0 +1,19 @@
+// XII·IX <> VII·X
+
+package nl.th7mo.trackshop.api.spotify.access_token;
+
+import nl.th7mo.trackshop.api.util.DotenvAdapter;
+
+import java.util.Base64;
+
+final class CredentialEncoder {
+
+    static String encodeBase64() {
+        String clientId = DotenvAdapter.get("CLIENT_ID");
+        String clientSecret = DotenvAdapter.get("CLIENT_SECRET");
+        Base64.Encoder encoder = Base64.getEncoder();
+        String clientCredentials = clientId + ":" + clientSecret;
+
+        return encoder.encodeToString(clientCredentials.getBytes());
+    }
+}
