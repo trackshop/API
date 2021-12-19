9b540d328f18fe9868769daedd3bb4e0a25fe32b
Th7mo <thimoquinten@gmail.com>
2021-12-19 15:20:32 +0100

    Make helper methods to retreive data from playlist more easy

--- src/main/java/nl/th7mo/trackshop/api/Application.java
+++ src/main/java/nl/th7mo/trackshop/api/Application.java
@@ -1,13 +1,7 @@
 package nl.th7mo.trackshop.api;
 
-import com.google.gson.Gson;
-import nl.th7mo.trackshop.api.spotify.access_token.AccessTokenDAO;
-import nl.th7mo.trackshop.api.spotify.playlist.Playlist;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @SpringBootApplication
 public class Application {
