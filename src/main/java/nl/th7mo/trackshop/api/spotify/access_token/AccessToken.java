2877b158fbd79dc60e6cae36281267d549527f89
Th7mo <thimoquinten@gmail.com>
2021-12-19 12:19:42 +0100

    Add AccessToken class that represents an AccessToken

---
+++ src/main/java/nl/th7mo/trackshop/api/spotify/access_token/AccessToken.java
@@ -0,0 +1,11 @@
+// XII·IX <> VII·X
+
+package nl.th7mo.trackshop.api.spotify.access_token;
+
+import com.google.gson.annotations.SerializedName;
+
+public final class AccessToken {
+
+    @SerializedName("access_token")
+    public String accessToken;
+}
