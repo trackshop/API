42c3d68e21d00da540e92582e5c865d490de5fed
th7mo <dev@th7mo.com>
2021-12-21 11:37:07 +0100

    Made playlist store all the tracks, not the first 100

--- src/main/java/nl/th7mo/trackshop/api/spotify/playlist/SpotifyTracks.java
+++ src/main/java/nl/th7mo/trackshop/api/spotify/playlist/SpotifyTracks.java
@@ -7,4 +7,9 @@ import java.util.List;
 public class SpotifyTracks {
 
     public List<SpotifyItem> items;
+    public String next;
+
+    public void concat(SpotifyTracks tracks) {
+        items.addAll(tracks.items);
+    }
 }
