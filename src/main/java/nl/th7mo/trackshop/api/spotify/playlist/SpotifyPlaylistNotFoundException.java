8201df1ab9c909cabc2f2dbe703fb4dc2959692a
Th7mo <thimoquinten@gmail.com>
2021-12-21 10:45:25 +0100

    Make every exception log itself

--- src/main/java/nl/th7mo/trackshop/api/spotify/playlist/SpotifyPlaylistNotFoundException.java
+++ src/main/java/nl/th7mo/trackshop/api/spotify/playlist/SpotifyPlaylistNotFoundException.java
@@ -2,11 +2,14 @@
 
 package nl.th7mo.trackshop.api.spotify.playlist;
 
+import nl.th7mo.trackshop.api.util.LogErrorAdapter;
+
 import java.io.FileNotFoundException;
 
 public class SpotifyPlaylistNotFoundException extends FileNotFoundException {
 
     public SpotifyPlaylistNotFoundException(String errorMessage) {
         super(errorMessage);
+        LogErrorAdapter.log(this.getClass());
     }
 }
