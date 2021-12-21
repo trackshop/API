e157738d96ad7e118f24a2f488850899645dffc4
th7mo <dev@th7mo.com>
2021-12-21 09:29:42 +0100

    Moved @ResponseStatus to ExceptionHandler from exception class

--- src/main/java/nl/th7mo/trackshop/api/playlist/PlaylistExceptionHandler.java
+++ src/main/java/nl/th7mo/trackshop/api/playlist/PlaylistExceptionHandler.java
@@ -4,6 +4,7 @@ package nl.th7mo.trackshop.api.playlist;
 
 import nl.th7mo.trackshop.api.spotify.playlist.SpotifyPlaylistNotFoundException;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class PlaylistExceptionHandler {
 
     @ResponseStatus(
+        value = HttpStatus.NOT_FOUND,
         reason = "The id given is not a valid spotify playlist id or " +
                 "the playlist is private"
     )
--- src/main/java/nl/th7mo/trackshop/api/spotify/playlist/SpotifyPlaylistNotFoundException.java
+++ src/main/java/nl/th7mo/trackshop/api/spotify/playlist/SpotifyPlaylistNotFoundException.java
@@ -2,16 +2,8 @@
 
 package nl.th7mo.trackshop.api.spotify.playlist;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 import java.io.FileNotFoundException;
 
-@ResponseStatus(
-    value = HttpStatus.NOT_FOUND,
-    reason = "The id given is not a valid spotify playlist id or " +
-            "the playlist is private"
-)
 public class SpotifyPlaylistNotFoundException extends FileNotFoundException {
 
     public SpotifyPlaylistNotFoundException(String errorMessage) {
