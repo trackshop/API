72255834d5e28cb2865e908d478eeffa849e70ac
th7mo <dev@th7mo.com>
2021-12-21 10:45:25 +0100

    Make every exception log itself

--- src/main/java/nl/th7mo/trackshop/api/playlist/PlaylistNotFoundException.java
+++ src/main/java/nl/th7mo/trackshop/api/playlist/PlaylistNotFoundException.java
@@ -2,11 +2,14 @@
 
 package nl.th7mo.trackshop.api.playlist;
 
+import nl.th7mo.trackshop.api.util.LogErrorAdapter;
+
 import java.io.FileNotFoundException;
 
 public class PlaylistNotFoundException extends FileNotFoundException {
 
     public PlaylistNotFoundException(String errorMessage) {
         super(errorMessage);
+        LogErrorAdapter.log(this.getClass());
     }
 }
